da2142cfdf664b08e8c63a4c01e0831cf2aa9c9e
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-08 23:16:52 -0700

    std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -3,14 +3,15 @@
 # Date: 05/08/2024
 # Description: defining the Person class, get_age method, and the std_dev function.
 class Person:
-    """represent a person with name and age"""
+    """Represent a person with name and age."""
     def __init__(self, name, age):
-        """initialize a person with name and age"""
+        """Initialize a person object with the given name and age."""
         self.__name = name
         self.__age = age
     def get_age(self):
         return self.__age
 def std_dev(people):
+    """Calculate the standard deviation of ages in a list of person object."""
     if not people:
         return None
     # Calculate the mean of age
@@ -19,5 +20,5 @@ def std_dev(people):
     # Calculate the sum of squares of differences from the mean
     sum_squares_diff = sum((person.get_age() - mean_age) ** 2 for person in people)
     # Calculate the population of standard deviation
-    std_dev = (sum_squares_diff / len(people)) ** 0.5
-    return std_dev
+    std_deviation = (sum_squares_diff / len(people)) ** 0.5
+    return std_deviation
